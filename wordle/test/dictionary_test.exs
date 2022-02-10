593a9c52478ddf307e6cb63cdc5b05f196d77a72
Theo Harris <theo@alembic.com.au>
2022-02-11 09:04:15 +1100

    add tests for Dictionary validators

--- wordle/test/dictionary_test.exs
+++ wordle/test/dictionary_test.exs
@@ -1,3 +1,10 @@
 defmodule DictionaryTest do
   use ExUnit.Case
+  test "returns true for a valid guess word" do
+    assert Dictionary.validate_guess("house") == true
+  end
+
+  test "returns false for an invalid guess word" do
+    assert Dictionary.validate_guess("asdfg") == false
+  end
 end
