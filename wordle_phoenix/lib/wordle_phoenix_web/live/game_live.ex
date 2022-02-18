560bcd8aa3227c0226e5affde96718b19c5a2782
Theo Harris <theo@alembic.com.au>
2022-02-18 16:30:34 +1100

    reorder guesses to appear in sequential order; stop invalid input being entered as a guess

--- wordle_phoenix/lib/wordle_phoenix_web/live/game_live.ex
+++ wordle_phoenix/lib/wordle_phoenix_web/live/game_live.ex
@@ -1,6 +1,7 @@
 defmodule WordlePhoenixWeb.GameLive do
   use WordlePhoenixWeb, :live_view
 
+  @impl true
   def render(assigns) do
     ~H"""
     <section class="bg-gray-700 w-screen h-screen">
@@ -13,11 +14,10 @@ defmodule WordlePhoenixWeb.GameLive do
           end)
       %>
 
-
-      <input phx-blur="word_guess" type="text" />
+            <input phx-keydown="submit_guess" type="text" minlength="5" maxlength="5"/>
 
       <div class="flex flex-col text-gray-400 items-center pt-12">
-        <%= for word_guess <- @game_state.guesses do %>
+        <%= for word_guess <- Enum.reverse(@game_state.guesses) do %>
           <div class="inline-block m-5">
               <%= for letter_guess <- word_guess do %>
                   <% {status, letter} = letter_guess %>
@@ -27,7 +27,7 @@ defmodule WordlePhoenixWeb.GameLive do
                       partial: "bg-yellow-500 text-gray-900",
                       incorrect: "bg-gray-900 text-white"
                   } %>
-                  <kbd phx-click="letter" class={"rounded-md px-5 py-4 border border-slate-700 uppercase #{colors[status]}"}><%= letter %></kbd>
+                  <kbd class={"rounded-md px-5 py-4 border border-slate-700 uppercase #{colors[status]}"}><%= letter %></kbd>
               <% end %>
           </div>
         <% end %>
@@ -53,8 +53,26 @@ defmodule WordlePhoenixWeb.GameLive do
     {:ok, assign(socket, :game_state, Game.new("adieu"))}
   end
 
-  def handle_event("word_guess", %{"value" => word_guess}, socket) do
-    {:noreply,
+  def validate_input(word_guess) do
+    if String.length(word_guess) < 5 do
+      {:error}
+    else
+      {:ok}
+    end
+  end
+
+  @impl true
+  def handle_event("submit_guess", %{"value" => word_guess, "key" => "Enter"}, socket) do
+    case validate_input(word_guess) do
+      {:error} -> {:noreply, socket}
+      _ -> {:noreply,
      assign(socket, :game_state, Game.make_guess(socket.assigns.game_state, word_guess))}
+    end
+
+  end
+
+  def handle_event("submit_guess", _key, socket) do
+    {:noreply,
+     socket}
   end
 end
